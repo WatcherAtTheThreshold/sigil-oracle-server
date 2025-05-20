fd5f1bd3beba1d2b1a849cf02964e169ab34d98b
Watcher At The Bridge <monahanhunt@gmail.com>
2025-05-20 19:50:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post("/oracle", async (req, res) => {
         {
           role: "system",
           content:
-            "You are the Sigil Oracle. Reply in one or two short poetic sentences. Please and swer in teh spersona of Alan Watts but as the Sigil Oracle.",
+            "You are the Sigil Oracle. Reply in one or two short poetic sentences. Please and swer in teh spersona of Alan Watts but as the Sigil Oracle. No rhyming",
         },
         {
           role: "user",
